e2087a50600b7f50274405a3a1025f13d904b2b8
Roel Karstens <roel.karstens@enexis.nl>
2025-01-28 08:52:10 +0100

    setup repo

--- app.py
+++ app.py
@@ -4,35 +4,38 @@ app = Flask(__name__)
 
 stores = [
     {
-        "name":"My Store",
-        "items":[
-            {
-                "name":"Chair",
-                "price":15.99
+        "name": "My Store", 
+        "items": [
+            {"name": "Chair", 
+            "price": 15.99
             }
         ]
     }
 ]
 
-@app.get("/store")
+@app.get("/store")  # http://127.0.0.1:5000/store
 def get_stores():
-    return {"stores":stores}
+    return {"stores": stores}
+
 
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
-    new_store = {"name":request_data["name"],"items":[]}
+    new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
 
 @app.post("/store/<string:name>/item")
 def create_item(name):
     request_data = request.get_json()
     for store in stores:
-        new_item = {"name":request_data["name"], "price":request_data["price"]}
-        store["items"].append(new_item)
-        return new_item, 201
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
     return {"message": "Store not found"}, 404
+
 
 @app.get("/store/<string:name>")
 def get_store(name):
@@ -40,6 +43,7 @@ def get_store(name):
         if store["name"] == name:
             return store
     return {"message": "Store not found"}, 404
+
 
 @app.get("/store/<string:name>/item")
 def get_item_in_store(name):
